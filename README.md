b630b4cebc46196ff526eccdddff9f3c2321f6b2
Philippe Hässig <phil@nine.ch>
2017-08-22 13:16:40 +0200

    First basic styling with Bootstrap

---
+++ README.md
@@ -0,0 +1,12 @@
+# Springboard
+
+Springboard is a tiny, little front-end for your local Docker instance. It just presents you an overview over the running containers with their exposed ports. Ports are clickable so you can immediately open a dockerized web application in your browser.
+
+## Build
+
+Assuming you have installed Go, NodeJS and Webpack you can just run the following commands:
+
+    $ wepback
+    $ go run main.go
+
+The build process is very basic. It will be moved into a Docker container itself later.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const Uglify = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './assets'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+    new Uglify()
+  ]
+}
